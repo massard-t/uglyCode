5d537a2116f5e7fdae2aeeba7901506161258f2c
Theo Massard <massar_t@etna-alternance.net>
2017-09-16 10:04:07 +0200

    Clean the code a bit

--- base.py
+++ base.py
@@ -1,12 +1,23 @@
 #!/usr/bin/env python
-import sys,os
-def hey(avar):return 1
+"""A nice module"""
+import sys
+import os
+
+
+def hey(avar):
+    return 1
+
+
 def main():
     try:
         os.system(sys.argv[1:])
     except IndexError:
         print("No problem :)")
+
+
 def yy(notavar):
-    m = notavar
-    print(m)
+    myvar = notavar
+    print(myvar)
+
+
 if __name__ == '__main__': main()
